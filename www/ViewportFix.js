dde164eaa7a8403745c0a18531f0360c57efc213
Hongbo LU <gitawego@gmail.com>
2014-08-06 17:35:35 +0200

    fixme only for android 4.4

--- www/ViewportFix.js
+++ www/ViewportFix.js
@@ -1,8 +1,14 @@
 
-var exec = require('cordova/exec');
+var exec = require('cordova/exec'),isAndroid44;
+var m = navigator.appVersion.match(/Android\s(.*?);/);
+if(m){
+	isAndroid44 = parseFloat(m[1]) === 4.4;
+}
 module.exports = {
 	fixme:function(callback) {
-		
+		if(!isAndroid44){
+			return callback && callback(null,'ok');
+		}
 		exec(function(res){
 			callback && callback(null,res);
 		}, function(error){
